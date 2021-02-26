6bd0992aed06fe5d8e129e4a0f17d7d3ffbabcb1
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-02-26 16:24:25 +0900

    3回目の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,8 +48,8 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordは6文字以上の入力がないと登録できない' do
-        @user.password = '00000'
-        @user.password_confirmation = '00000'
+        @user.password = 'aa000'
+        @user.password_confirmation = 'aa000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
