86b5ce45367f12398614b34e1fb4345820551980
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-02-28 11:52:22 +0900

    販売手数料、利益の計算

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     validates :area_id
     validates :deliveryday_id
   end
-    validates :price, format:{ with: /\A[0-9]+\z/ }
+    validates :price, format:{ with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :user
     validates :image
   end
