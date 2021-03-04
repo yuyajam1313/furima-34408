4e7e7bfca6dc14b4bdb0e816dacb031cc8bdea6f
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-03-04 11:06:52 +0900

    ログアウトユーザーは、購入画面へ遷移できず、自分で投稿した購入画面へは遷移できない実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,12 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index]
+ 
   def index
    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id
+       redirect_to root_path
+    end
   end
+  
 end
 
